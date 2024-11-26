6806822e8e0fbf4e199cb49ce412823fce7b9656
prodesk98 <profissional.mateus.andrade@gmail.com>
2024-11-26 01:52:45 -0300

    feat: edited sound, adjusted GameRoom, dialog game leave

--- client/src/components/GameRoom.tsx
+++ client/src/components/GameRoom.tsx
@@ -1,10 +1,16 @@
 import {useEffect, useRef, useState} from "react";
-//assets
+// assets
 import accept from "../assets/icons/accept.png";
 import incorrect from "../assets/icons/incorrect.png";
 import trophy from "../assets/icons/trophy.png";
-import correctSound from "../assets/sounds/correct-answer.wav";
-import backgroundMusicGameTimer from "../assets/sounds/music-game-timer.wav";
+import correctSound from "../assets/sounds/correct-answer.ogg";
+import incorrectSound from "../assets/sounds/incorrect-answer.ogg";
+import backgroundMusicGameTimer from "../assets/sounds/music-game-timer.ogg";
+
+// icons
+import VolumeOff from "@mui/icons-material/VolumeOff"
+import VolumeUp from "@mui/icons-material/VolumeUp"
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 // css
 import "../css/GameRoom.css";
@@ -17,7 +23,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function GameRoom(){
     const [timeLeft, setTimeLeft] = useState(30); // 30 segundos para responder
+    const [isMuted, setIsMuted] = useState(false); // Mudo ou não
+    const [answerSelected, setAnswerSelected] = useState(null); // Resposta selecionada
+    const [showDialog, setShowDialog] = useState(false); // Exibe o diálogo de saída
     const totalTime = 30; // Tempo total da pergunta
+    const correctAnswerIndex = 2; // Resposta correta
+    const options = ["C++", "C#", "Python", "Javascript"]; // Opções de resposta
+
 
     // Efeito sonoro de resposta correta
     const correctSoundEffect = useRef(() => {
@@ -28,11 +40,20 @@ export default function GameRoom(){
         return audio;
     });
 
+    // Efeito sonoro de resposta incorreta
+    const incorrectSoundEffect = useRef(() => {
+        const audio = new Audio(incorrectSound);
+        audio.volume = 0.8;
+        audio.currentTime = 0;
+        audio.loop = false;
+        return audio;
+    });
+
     // Lista de jogadores
     const players = [
-        { id: 1, name: "Player1", avatar: "https://i.pravatar.cc/54", accept: true, best: false },
-        { id: 2, name: "Player2", avatar: "https://i.pravatar.cc/54", accept: false, best: false },
+        { id: 1, name: "Você", avatar: "https://i.pravatar.cc/54", accept: true, best: false },
         { id: 3, name: "Player3", avatar: "https://i.pravatar.cc/54", accept: true, best: true },
+        { id: 2, name: "Player2", avatar: "https://i.pravatar.cc/54", accept: false, best: false },
         { id: 4, name: "Player4", avatar: "https://i.pravatar.cc/54", accept: false, best: false },
         { id: 5, name: "Player5", avatar: "https://i.pravatar.cc/54", accept: false, best: false },
     ];
@@ -48,14 +69,33 @@ export default function GameRoom(){
     // TODO: Purchase the full track: https://1.envato.market/WDQBgA
     useEffect(() => {
         const audio = new Audio(backgroundMusicGameTimer);
-        audio.loop = true;
-        audio.volume = 0.08;
-        audio.play().then(() => console.log("Background music started!"));
+        audio.loop = false;
+        audio.volume = isMuted ? 0 : 0.08; // Define o volume com base no estado 'isMuted'
+
+        if (!isMuted) {
+            audio.play().then(() => console.log("Background music started!"));
+        } else {
+            audio.pause();
+        }
+
+        audio.currentTime = 30 - timeLeft; // Define o tempo atual com base no tempo restante
+
         return () => {
             audio.pause();
-            audio.currentTime = 0;
         };
-    }, []);
+    }, [isMuted]);
+
+    // Seleciona a resposta
+    const handleSelectAnswer = (i: number) => {
+        // @ts-ignore
+        setAnswerSelected(i);
+    };
+    useEffect(() => {
+        if(answerSelected !== null){
+            // TODO: Enviar a resposta para o servidor
+            console.log(`Answer selected: ${answerSelected}`);
+        }
+    }, [answerSelected]);
 
     const handleNotifyError = (e: string) => toast.error(e, {
         position: "bottom-right",
@@ -81,81 +121,155 @@ export default function GameRoom(){
         });
     };
 
-    const handleAnswer = (e: any) => {
+    const handleAnswer = (e: any, i: number) => {
         if (timeLeft <= 0) {
             handleNotifyError(`${timeLeft} seconds left to answer!`);
             return;
         }
-
-        correctSoundEffect.current().play().then(() => console.log("Correct answer!"));
-        handleConfetti(e);
+        if (answerSelected !== null) {
+            handleNotifyError("You have already selected an answer!");
+            return;
+        }
+        if (i === correctAnswerIndex){
+            if (!isMuted) correctSoundEffect.current().play().then(() => console.log("Correct answer!"));
+            handleConfetti(e);
+        }
+        else{
+            if (!isMuted) incorrectSoundEffect.current().play().then(() => console.log("Incorrect answer!"));
+        }
+        handleSelectAnswer(i);
     }
+
+    const handleLeaveGame = () => {};
+
+    const handleExitClick = () => {
+        setShowDialog(true); // Exibe o diálogo
+    };
+
+    const confirmExit = () => {
+        console.log("Usuário saiu do jogo!");
+        //TODO:  Adicione aqui a lógica para sair do jogo, como redirecionar para a home
+        // window.location.href = "/home";
+        handleLeaveGame();
+    };
+
+    const cancelExit = () => {
+        setShowDialog(false); // Oculta o diálogo
+    };
+
+    const handleToggleSound = () => {
+        const toggle = !isMuted;
+        setIsMuted(toggle);
+
+        if (toggle) {
+            correctSoundEffect.current().pause();
+        }
+    };
 
     const progressPercentage = (timeLeft / totalTime) * 100;
 
     return (
-        <div className="quiz-wrapper">
-            <div className="quiz-container">
-                {/* Lista de jogadores no topo */}
-                <div className="players-list">
-                    {players.map((player) => (
-                        <div className={`player ${player.best ? "best-player" : ""}`} key={player.id}>
-                            <div className="avatar-container">
-                                <img
-                                    src={`${player.avatar}?img=${player.id}`}
-                                    alt={player.name}
-                                    className="player-avatar"
-                                />
-                                {player.accept ? (
+        <>
+            <div className="quiz-wrapper">
+                <div className="quiz-container">
+                    {/* Lista de jogadores no topo */}
+                    <div className="players-list">
+                        {players.map((player) => (
+                            <div className={`player ${player.best ? "best-player" : ""}`} key={player.id}>
+                                <div className="avatar-container">
                                     <img
-                                        src={accept}
-                                        alt="Correct"
-                                        className="icon-correct"
+                                        src={`${player.avatar}?img=${player.id}`}
+                                        alt={player.name}
+                                        className="player-avatar"
                                     />
-                                ) : (
-                                    <img
-                                        src={incorrect}
-                                        alt="Incorrect"
-                                        className="icon-incorrect"
-                                    />
-                                )}
-                                {player.best && (
-                                    <img
-                                        src={trophy}
-                                        alt="Best Player"
-                                        className="icon-trophy"
-                                    />
-                                )}
+                                    {player.accept ? (
+                                        <img
+                                            src={accept}
+                                            alt="Correct"
+                                            className="icon-correct"
+                                        />
+                                    ) : (
+                                        <img
+                                            src={incorrect}
+                                            alt="Incorrect"
+                                            className="icon-incorrect"
+                                        />
+                                    )}
+                                    {player.best && (
+                                        <img
+                                            src={trophy}
+                                            alt="Best Player"
+                                            className="icon-trophy"
+                                        />
+                                    )}
+                                </div>
+                                <span className="player-name">{player.name}</span>
                             </div>
-                            <span className="player-name">{player.name}</span>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
 
-                {/* Barra de progresso */}
-                <div className="progress-bar">
-                    <div
-                        className="progress-bar-fill"
-                        style={{
-                            width: `${progressPercentage}%`,
-                            background: timeLeft > 10 ? "#4caf50" : "#ff5555",
-                        }}
-                    ></div>
-                </div>
+                    {/* Barra de progresso */}
+                    <div className="progress-bar">
+                        <div
+                            className="progress-bar-fill"
+                            style={{
+                                width: `${progressPercentage}%`,
+                                background: timeLeft > 10 ? "#4caf50" : "#ff5555",
+                            }}
+                        ></div>
+                    </div>
 
-                {/* Pergunta */}
-                <div className="question">
-                    Qual a melhor linguagem de programação de todos os tempos?
-                </div>
+                    {/* Pergunta */}
+                    <div className="question">
+                        Qual a melhor linguagem de programação de todos os tempos?
+                    </div>
 
-                {/* Opções */}
-                <div className="options">
-                    <button className="option" onClick={handleAnswer}>C++</button>
-                    <button className="option" onClick={handleAnswer}>C#</button>
-                    <button className="option correct" onClick={handleAnswer}>Python</button>
-                    <button className="option" onClick={handleAnswer}>Javascript</button>
+                    {/* Opções */}
+                    <div className="options">
+                        {options.map((option, index) => (
+                            <button
+                                key={index}
+                                className={`option ${
+                                    answerSelected !== null
+                                        ? index === correctAnswerIndex
+                                            ? "correct" // Destaca a resposta correta
+                                            : index === answerSelected
+                                                ? "incorrect" // Destaca a resposta errada escolhida
+                                                : ""
+                                        : ""
+                                }`}
+                                onClick={(e) => handleAnswer(e, index)}
+                                disabled={answerSelected !== null} // Desativa os botões após a escolha
+                            >
+                                {option}
+                            </button>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="top-right-buttons">
+                <button className="btn-mute" onClick={() => handleToggleSound()}>
+                    {isMuted ? <VolumeOff /> : <VolumeUp />}
+                </button>
+                <button className="btn-exit" onClick={() => handleExitClick()}>
+                    <ExitToAppIcon />
+                </button>
+            </div>
+            {showDialog && (
+                <div className="dialog-overlay">
+                    <div className="dialog">
+                        <p>Tem certeza de que deseja sair da sala?</p>
+                        <div className="dialog-actions">
+                            <button className="btn-confirm" onClick={confirmExit}>
+                                Sim
+                            </button>
+                            <button className="btn-cancel" onClick={cancelExit}>
+                                Cancelar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
